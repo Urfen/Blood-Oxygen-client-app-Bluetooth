52609840738039d8f37d4912800c9b39b570f04e
Urfen <urfen93@gmail.com>
2015-12-06 13:30:09 +0100

    Skicka sträng funkar

---
+++ app/src/main/java/se/arvidbodkth/laboration3b/DataSender.java
@@ -0,0 +1,51 @@
+package se.arvidbodkth.laboration3b;
+
+import android.nfc.FormatException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Arvid Bodin och Mattias Grehnik on 2015-12-06.
+ *
+ */
+public class DataSender {
+
+    private String ipAddress;
+    private static final int port = 8008;
+    private String data;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Socket socket;
+
+    public DataSender(String data){
+        ipAddress = "192.168.0.14";
+        this.data = data;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public void sendData(){
+        try{
+            socket = new Socket(ipAddress,port);
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+
+            out.println(data);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/se/arvidbodkth/laboration3b/MainActivity.java
+++ app/src/main/java/se/arvidbodkth/laboration3b/MainActivity.java
@@ -1,15 +1,21 @@
 package se.arvidbodkth.laboration3b;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private DataSender model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                BluetoothTask bluetoothTask = new BluetoothTask();
+                bluetoothTask.execute();
+
             }
         });
+
+    }
+
+    public void showToast(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     @Override
@@ -49,4 +64,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private class BluetoothTask extends AsyncTask<Void,Void,String>{
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            //Hämta BL data
+
+            //Spara data
+
+            return "Testtes123";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            TcpTask tcpTask = new TcpTask();
+            tcpTask.execute(s);
+
+        }
+    }
+
+    private class TcpTask extends AsyncTask<String,Void,Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+
+            DataSender dataSender = new DataSender(params[0]);
+            dataSender.sendData();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            System.out.println("TcpTask klar.");
+        }
+    }
 }
+
